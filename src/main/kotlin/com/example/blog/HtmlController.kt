ee7f473483e605cc4ccade7d79ce1f95cede61f4
chanqun <cksgns93@naver.com>
2021-05-15 14:36:51 +0900

    blog main화면

--- src/main/kotlin/com/example/blog/HtmlController.kt
+++ src/main/kotlin/com/example/blog/HtmlController.kt
@@ -2,20 +2,15 @@ package com.example.blog
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class HtmlController {
-    @GetMapping
-    fun controllerName(model: Model): String {
 
-        return "view"
-    }
-
-    @PostMapping("/")
-    fun (model: Model): String {
-
-        return "view"
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = "Blog"
+        return "blog"
     }
 }
